78830fbed3bcad03a073547b805a1e4d727a0e6d
mynamerahulkumar <mynamerahulkumar13@gmail.com>
2018-02-24 04:22:17 +0530

    linked list and tree

---
+++ data_structure/linkedList/postorder_traversal.java
@@ -0,0 +1,23 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+void postOrder(Node root) {
+    /* 
+       left,right,root
+    */
+    if(root!=null){
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+}
+
